f9f4f5471add7160b8c4b751de6e73bdfd4d3048
正经吉 <dening1644@163.com>
2019-02-11 14:34:48 +0800

    正经吉公众号增加filter

---
+++ src/main/java/com/jidn/web/filter/WeChatFilter.java
@@ -0,0 +1,29 @@
+package com.jidn.web.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @Copyright © 北京互融时代软件有限公司
+ * @Author: Jidn
+ * @Date: 2019/1/22 15:27
+ * @Description: 微信过滤
+ */
+public class WeChatFilter implements Filter {
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
